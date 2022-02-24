33a297ba692e3e03b0b846a5f2d9d5e77f928edd
Anders Östlin <anders.ostlin@gmail.com>
2022-02-24 14:47:56 +0100

    Flips bases of rotation matrix and flips pitch and roll on quaternion

--- Tests/ComposableCoreMotionTests/ComposableCoreMotionTests.swift
+++ Tests/ComposableCoreMotionTests/ComposableCoreMotionTests.swift
@@ -31,20 +31,47 @@ class ComposableCoreMotionTests: XCTestCase {
     let q3 = Attitude(quaternion: .init(x: 0, y: 0, z: 1, w: 0))
     let q4 = Attitude(quaternion: .init(x: 0, y: 0, z: 0, w: 1))
 
-    XCTAssertEqual(q1.roll, Double.pi)
-    XCTAssertEqual(q1.pitch, 0)
+    XCTAssertEqual(q1.roll, 0)
+    XCTAssertEqual(q1.pitch, 180.0)
     XCTAssertEqual(q1.yaw, 0)
 
-    XCTAssertEqual(q2.roll, Double.pi)
-    XCTAssertEqual(q2.pitch, 0)
-    XCTAssertEqual(q2.yaw, Double.pi)
+    XCTAssertEqual(q2.roll, 0)
+    XCTAssertEqual(q2.pitch, 180.0)
+    XCTAssertEqual(q2.yaw, 180.0)
 
     XCTAssertEqual(q3.roll, 0)
     XCTAssertEqual(q3.pitch, 0)
-    XCTAssertEqual(q3.yaw, Double.pi)
+    XCTAssertEqual(q3.yaw, 180.0)
 
     XCTAssertEqual(q4.roll, 0)
     XCTAssertEqual(q4.pitch, 0)
     XCTAssertEqual(q4.yaw, 0)
+  }
+  
+  func testRotationMatrix() {
+    let q = CMQuaternion(x: 1, y: 2, z: 3, w: 4)
+    let a = Attitude(quaternion: q)
+    
+    let m = a.rotationMatrix
+    
+    XCTAssertEqual(m.m11, 0.13333333, accuracy: 0.0001)
+    XCTAssertEqual(m.m12, 0.93333333, accuracy: 0.0001)
+    XCTAssertEqual(m.m13, 0.73333333, accuracy: 0.0001)
+    
+    XCTAssertEqual(m.m21, -0.6666666, accuracy: 0.0001)
+    XCTAssertEqual(m.m22, 0.33333333, accuracy: 0.0001)
+    XCTAssertEqual(m.m23, 0.66666666, accuracy: 0.0001)
+
+    XCTAssertEqual(m.m31, -0.33333333, accuracy: 0.0001)
+    XCTAssertEqual(m.m32, 0.13333333, accuracy: 0.0001)
+    XCTAssertEqual(m.m33, 0.66666666, accuracy: 0.0001)
+  }
+
+  func testCompability() {
+    let q = CMQuaternion(x: -0.000882, y: -0.005251, z: 0.932196, w: -0.361915)
+    let a = Attitude(quaternion: q)
+    XCTAssertEqual(a.pitch, -0.524317, accuracy: 0.0001)
+    XCTAssertEqual(a.roll, 0.312014, accuracy: 0.0001)
+    XCTAssertEqual(a.yaw, -137.562226, accuracy: 0.01)
   }
 }
